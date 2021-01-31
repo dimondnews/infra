c7c50a85051d8beede688af139be834e4c0a5626
Tomasz Finc <tomasz@gmail.com>
2021-01-30 17:54:34 -0800

    Making rotate certs more extensible

--- rotate-certs.sh
+++ rotate-certs.sh
@@ -1,13 +1,33 @@
-DIRS=(bitnami)
 DOMAIN=dimondnews.org
+WILDCARD=*.$DOMAIN
+DIRS=(bitnami)
 
-for dir in ${DIRS[@]}
-do
-        echo ${dir}
-        sudo /opt/${dir}/ctlscript.sh stop apache
-        sudo mv /opt/${dir}/apache2/conf/server.crt /opt/${dir}/apache2/conf/server.crt.old
-        sudo mv /opt/${dir}/apache2/conf/server.key /opt/${dir}/apache2/conf/server.key.old
-        sudo ln -s /etc/letsencrypt/live/${DOMAIN}/privkey.pem /opt/${dir}/apache2/conf/server.key
-        sudo ln -s /etc/letsencrypt/live/${DOMAIN}/fullchain.pem /opt/${dir}/apache2/conf/server.crt
-        sudo /opt/${dir}/ctlscript.sh start apache
+renew() {
+  sudo certbot -d $DOMAIN -d $WILDCARD --manual --preferred-challenges dns certonly
+}
+
+move() {
+  for dir in ${DIRS[@]}
+  do
+          echo ${dir}
+          sudo /opt/${dir}/ctlscript.sh stop apache
+
+          sudo mv /opt/${dir}/apache2/conf/server.crt /opt/${dir}/apache2/conf/server.crt.old
+          sudo mv /opt/${dir}/apache2/conf/server.key /opt/${dir}/apache2/conf/server.key.old
+          sudo ln -s /etc/letsencrypt/live/${DOMAIN}/privkey.pem /opt/${dir}/apache2/conf/server.key
+          sudo ln -s /etc/letsencrypt/live/${DOMAIN}/fullchain.pem /opt/${dir}/apache2/conf/server.crt
+          sudo /opt/${dir}/ctlscript.sh start apache
+  done
+}
+
+while getopts "rm" option; do
+ case "${option}" in
+    r) action=renew;;
+    m) action=move;;
+  esac
 done
+
+case "$action" in
+  "renew") renew;;
+  "move") move;;
+esac
